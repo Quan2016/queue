f042c628f7b28c48de5b32971eff19cd53191a53
hunter <1346788454@qq.com>
2019-03-24 16:24:35 +0800

    update disruptor

--- src/main/java/com/spider/queue/disruptor/MessageEventHandler.java
+++ src/main/java/com/spider/queue/disruptor/MessageEventHandler.java
@@ -2,11 +2,15 @@ package com.spider.queue.disruptor;
 
 import com.lmax.disruptor.EventHandler;
 
+/**
+ * 消费者
+ * @author jq
+ */
 public class MessageEventHandler implements EventHandler<MessageEvent>{
 
 	@Override
 	public void onEvent(MessageEvent messageEvent,long sequence, boolean endOfBatch) throws Exception {
-		System.out.println(messageEvent);
+		System.out.println("consume message:" + messageEvent.getMessage() + " sequence:" + sequence + " endOfBatch:" + endOfBatch);
 	}
 
 }
---
+++ src/test/java/com/spider/queue/disruptor/MessageEventTest.java
@@ -0,0 +1,29 @@
+package com.spider.queue.disruptor;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import com.lmax.disruptor.dsl.Disruptor;
+import com.lmax.disruptor.util.DaemonThreadFactory;
+
+public class MessageEventTest {
+	MessageEventProducer producer = null;	
+	@Before
+	public void init(){
+		int bufferSize = 1024;//bufferSize必须是2的幂
+		Disruptor<MessageEvent> disruptor = new Disruptor<MessageEvent>(new MessageEventFactory(), bufferSize, DaemonThreadFactory.INSTANCE);
+		disruptor.handleEventsWith(new MessageEventHandler());//定义由谁消费信息
+//		disruptor.handleEventsWith(new MessageEventHandler());//定义由谁消费信息
+		disruptor.start();
+		producer = new MessageEventProducer(disruptor.getRingBuffer());
+	}
+	@Test
+	public void productMessage(){
+		String msg = "this is messg in sequence";
+		for(int i = 0;i < 10;i++){
+			MessageEvent messageEvent = new MessageEvent();
+			messageEvent.setMessage(msg + i);
+			producer.put(messageEvent);
+		}
+	}
+}
